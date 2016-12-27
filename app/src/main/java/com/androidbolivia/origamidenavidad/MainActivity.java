18b51edc1cbe0982829cd7708bff17d3aed4e3c4
KeGio31 <kegio.dirq@gmail.com>
2016-12-27 13:11:17 -0400

    App con botones y animaciones

--- app/src/main/java/com/androidbolivia/origamidenavidad/MainActivity.java
+++ app/src/main/java/com/androidbolivia/origamidenavidad/MainActivity.java
@@ -1,13 +1,43 @@
 package com.androidbolivia.origamidenavidad;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+private ImageView imNav;
+    private Animation animacion;
+    private MediaPlayer sound;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imNav=(ImageView) findViewById(R.id.arbol_navi);
+        sound=MediaPlayer.create(getApplicationContext(),R.raw.boton);
+    }
+    public void rot(View view){
+        sound.start();
+        animacion = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotacion);
+        imNav.startAnimation(animacion);
+
+
+    }
+    public void zoom(View view){
+        sound.start();
+        animacion = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom);
+        imNav.startAnimation(animacion);
+
+
+    }
+    public void desvanecer(View view){
+        sound.start();
+        animacion = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.desv);
+        imNav.startAnimation(animacion);
+
+
     }
 }
